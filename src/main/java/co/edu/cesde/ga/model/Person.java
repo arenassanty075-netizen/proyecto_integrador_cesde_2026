1e736e22bbb738306f244f2473938299adf85602
SantiagoArenas <arenassanty075@gmail.com>
2026-02-24 08:00:02 -0500

    Creacion de model o clase person

---
+++ src/main/java/co/edu/cesde/ga/model/Person.java
@@ -0,0 +1,12 @@
+package co.edu.cesde.ga.model;
+
+public class Person {
+    long userId;
+    String code;
+    String documentNumber;
+    String firsName;
+    String lastName;
+    String status;
+
+
+}
