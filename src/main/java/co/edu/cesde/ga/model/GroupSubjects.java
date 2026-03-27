5a9d47c341135f251315a20f3b81805d50a9fdb0
SantiagoArenas <arenassanty075@gmail.com>
2026-03-27 10:50:08 -0500

    Corrección subjectId a String en GroupSubjects y ajuste de repositorio

--- src/main/java/co/edu/cesde/ga/model/GroupSubjects.java
+++ src/main/java/co/edu/cesde/ga/model/GroupSubjects.java
@@ -1,30 +1,51 @@
 package co.edu.cesde.ga.model;
 
 public class GroupSubjects {
+
     private long groupSubjectId;
     private long groupId;
-    private long subjectId;
+    private String subjectId; // 🔥 CAMBIO AQUÍ
     private long teacherId;
 
     public GroupSubjects() {
     }
 
-    public GroupSubjects(long groupSubjectId, long groupId, long subjectId, long teacherId) {
+    public GroupSubjects(long groupSubjectId, long groupId, String subjectId, long teacherId) {
         this.groupSubjectId = groupSubjectId;
         this.groupId = groupId;
         this.subjectId = subjectId;
         this.teacherId = teacherId;
     }
 
-    public long getGroupSubjectId() { return groupSubjectId; }
-    public void setGroupSubjectId(long groupSubjectId) { this.groupSubjectId = groupSubjectId; }
+    public long getGroupSubjectId() {
+        return groupSubjectId;
+    }
 
-    public long getGroupId() { return groupId; }
-    public void setGroupId(long groupId) { this.groupId = groupId; }
+    public void setGroupSubjectId(long groupSubjectId) {
+        this.groupSubjectId = groupSubjectId;
+    }
 
-    public long getSubjectId() { return subjectId; }
-    public void setSubjectId(long subjectId) { this.subjectId = subjectId; }
+    public long getGroupId() {
+        return groupId;
+    }
 
-    public long getTeacherId() { return teacherId; }
-    public void setTeacherId(long teacherId) { this.teacherId = teacherId; }
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(long teacherId) {
+        this.teacherId = teacherId;
+    }
 }
---
+++ src/main/java/co/edu/cesde/ga/repository/impl/GroupSubjectsRepositoryInMemory.java
@@ -0,0 +1,86 @@
+package co.edu.cesde.ga.repository.impl;
+
+import co.edu.cesde.ga.model.GroupSubjects;
+import co.edu.cesde.ga.repository.GroupSubjectsRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupSubjectsRepositoryInMemory implements GroupSubjectsRepository {
+
+    private List<GroupSubjects> groupSubjects;
+
+    public GroupSubjectsRepositoryInMemory() {
+        this.groupSubjects = new ArrayList<>();
+    }
+
+    @Override
+    public GroupSubjects create(GroupSubjects groupSubject) {
+        if (groupSubject == null) {
+            return null;
+        }
+
+        if (exists(groupSubject.getGroupId(), groupSubject.getSubjectId())) {
+            return null;
+        }
+
+        groupSubjects.add(groupSubject);
+        return groupSubject;
+    }
+
+    @Override
+    public boolean exists(long groupId, String subjectId) {
+        return findByIds(groupId, subjectId) != null;
+    }
+
+    @Override
+    public GroupSubjects findByIds(long groupId, String subjectId) {
+        if (subjectId == null || subjectId.isBlank()) {
+            return null;
+        }
+
+        for (GroupSubjects gs : groupSubjects) {
+            if (gs.getGroupId() == groupId &&
+                    subjectId.equals(gs.getSubjectId())) {
+                return gs;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<GroupSubjects> findAll() {
+        return new ArrayList<>(groupSubjects);
+    }
+
+    @Override
+    public boolean delete(long groupId, String subjectId) {
+        GroupSubjects gs = findByIds(groupId, subjectId);
+        if (gs == null) return false;
+
+        groupSubjects.remove(gs);
+        return true;
+    }
+
+    @Override
+    public int count() {
+        return groupSubjects.size();
+    }
+
+    @Override
+    public boolean update(GroupSubjects updated) {
+        if (updated == null) return false;
+
+        for (int i = 0; i < groupSubjects.size(); i++) {
+            GroupSubjects current = groupSubjects.get(i);
+
+            if (current.getGroupId() == updated.getGroupId() &&
+                    current.getSubjectId().equals(updated.getSubjectId())) {
+
+                groupSubjects.set(i, updated);
+                return true;
+            }
+        }
+        return false;
+    }
+}
